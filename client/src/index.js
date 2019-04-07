a5bd2721b9db4fe3895a2bd44da819a0da79e15c
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-07 21:54:49 +0700

    create context, ProtectedRoute

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 
 import Router from "./routes";
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router />
-  </ApolloProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Router />, document.getElementById("root"));
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -8,15 +8,33 @@ import Signout from "./components/Signout";
 import PageNotFound from "./components/PageNotFound";
 import Blog from "./components/Blog";
 
-export default () => (
-  <Router>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/blog" component={Blog} />
-      <Route path="/signin" component={Signin} />
-      <Route path="/signout" component={Signout} />
-      <Route component={PageNotFound} />
-    </Switch>
-  </Router>
-);
+import ProtectedRoute from "./ProtectedRoute";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import Context from "./context";
+import reducer from "./reducer";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
+
+export default () => {
+  const initialState = useContext(Context);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <Router>
+      <ApolloProvider client={client}>
+        <Context.Provider value={{ state, dispatch }}>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <ProtectedRoute path="/blog" component={Blog} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/signout" component={Signout} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </Context.Provider>
+      </ApolloProvider>
+    </Router>
+  );
+};
