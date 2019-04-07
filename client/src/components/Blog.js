fa60545b6ec5166b760d77ac9fd41ca16deab4bb
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-07 20:33:32 +0700

    apply useState, useEffect in Blog component

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Blog = () => <div>Blog</div>;
+const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    const posts = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    setBlogs(posts.data);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div>
+      <h2>Blog</h2>
+      {blogs.length > 0 && (
+        <ul>
+          {blogs.map(post => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Signin = () => (
-  <div>
-    <h2>Signin</h2>
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-        console.log("submited");
-      }}
-    >
-      <div>
-        <label htmlFor="Username">Username </label>
-        <input type="text" placeholder="Username" />
-      </div>
-      <div>
-        <label htmlFor="Password">Password </label>
-        <input type="password" placeholder="********" />
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  </div>
-);
+const Signin = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  return (
+    <div>
+      <h2>Signin</h2>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          console.log(username);
+          console.log(password);
+        }}
+      >
+        <div>
+          <label htmlFor="Username">Username </label>
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={e => {
+              setUsername(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="Password">Password </label>
+          <input
+            type="password"
+            placeholder="********"
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Signin;
